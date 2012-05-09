dc11209fd851abff15ac9f9d02d54c69b00219f7
andreikolotsei <andreismith69@gmail.com>
2012-05-10 02:19:56 +0300

    10.05.2012/4/16/refactorings.history

---
+++ scanner/src/scanner/namespace/Maps.java
@@ -0,0 +1,59 @@
+package scanner.namespace;
+
+import java.util.List;
+
+import com.google.android.maps.GeoPoint;
+import com.google.android.maps.MapActivity;
+import com.google.android.maps.MapController;
+import com.google.android.maps.MapView;
+import com.google.android.maps.Overlay;
+import com.google.android.maps.OverlayItem;
+
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle; 
+import android.view.Window;
+
+public class Maps extends MapActivity {
+	
+	public MapView mapView;
+    public MapController mapController;
+    
+    List<Overlay> mapOverlays;
+    Drawable drawable;
+    MyItemizedOverlay itemizedoverlay;
+	
+	 /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE); //скрываем заголовок
+        setContentView(R.layout.map);
+        
+        //инициализируем карту
+        mapView = (MapView) findViewById(R.id.mapView);
+        //добавляем стандартные кнопки зума
+        mapView.setBuiltInZoomControls(true);
+        mapController = mapView.getController();
+        
+        //создаём GeoPoint с координатами 
+        Intent intent = getIntent();
+        GeoPoint point = new GeoPoint((int) (intent.getDoubleExtra("latitude", 0) * 1E6), (int) (intent.getDoubleExtra("longitude", 0)* 1E6));
+        //и передвигаем карту в эту точку
+        mapController.animateTo(point);
+        mapController.setZoom(16);
+        
+        mapOverlays = mapView.getOverlays();
+        drawable =  this.getResources().getDrawable(R.drawable.ic_launcher);
+        itemizedoverlay = new MyItemizedOverlay(drawable);
+        OverlayItem overlayitem = new OverlayItem(point, "@string/a", "s");
+        itemizedoverlay.addOverlay(overlayitem);
+        mapOverlays.add(itemizedoverlay);
+    }
+ 
+	 @Override
+	 protected boolean isRouteDisplayed() {
+	  // TODO Auto-generated method stub
+	  return false;
+	 }
+}
---
+++ scanner/src/scanner/namespace/MyItemizedOverlay.java
@@ -0,0 +1,32 @@
+package scanner.namespace;
+
+import java.util.ArrayList;
+
+import android.graphics.drawable.Drawable;
+
+import com.google.android.maps.ItemizedOverlay;
+import com.google.android.maps.OverlayItem;
+
+public class MyItemizedOverlay extends ItemizedOverlay {
+	private ArrayList<OverlayItem> mOverlays = new ArrayList<OverlayItem>();
+
+	public MyItemizedOverlay(Drawable defaultMarker) {
+		super(boundCenterBottom(defaultMarker));
+	}
+
+	public void addOverlay(OverlayItem overlay) {
+	    mOverlays.add(overlay);
+	    populate();
+	}
+	
+	@Override
+	protected OverlayItem createItem(int i) {
+	  return mOverlays.get(i);
+	}
+
+	@Override
+	public int size() {
+		return mOverlays.size();
+	}
+
+}
